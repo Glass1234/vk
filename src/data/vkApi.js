0cd08861e68e92c70462cfb3ea5407ff9a3c7bbc
ivalera <stepaniuk2016@yandex.ru>
2023-07-08 14:34:48 +0300

    Изменил формирование запросов на URLSearchParams

--- src/data/vkApi.js
+++ src/data/vkApi.js
@@ -5,9 +5,24 @@ import store from "@/store";
 export class Api {
     #token = token.token
     #baseUrl = 'https://api.vk.com/method/'
+    #baseDataUrl = {
+        access_token: this.#token,
+        v: 5.131
+    }
+
+    #createURL(method, data) {
+        let url = new URL(this.#baseUrl + method)
+        for (let key in this.#baseDataUrl) {
+            url.searchParams.set(key, this.#baseDataUrl[key])
+        }
+        for (let key in data) {
+            url.searchParams.set(key, data[key])
+        }
+        return url
+    }
 
     async #getRequest(apiMethod, data = null) {
-        const url = `${this.#baseUrl + apiMethod}?access_token=${this.#token}&v=5.131${data === null ? '' : data}`
+        const url = this.#createURL(apiMethod, data)
         return (await axios({
             method: 'get',
             url: url
@@ -21,43 +36,62 @@ export class Api {
 
     // возвращает список друзей
     async getUserPeople(id) {
-        const data = `&order=hints&user_id=${id}`
+        const data = {
+            order: 'hints',
+            user_id: id
+        }
         return await this.#getRequest('friends.get', data)
     }
 
     // возвращает список сообществ
     async getUserCountCommunitiles(id) {
-        const data = `&user_id=${id}`
+        const data = {
+            user_id: id
+        }
         return await this.#getRequest('groups.get', data)
     }
 
     // возвращает список публичных страниц + список групп
     async getUserCountGroups(id) {
-        const data = `&user_id=${id}`
+        const data = {
+            user_id: id
+        }
         return await this.#getRequest('users.getSubscriptions', data)
     }
 
     // возвращает список пользователей онлайн
     async getFriendsOnline(id) {
-        const data = `&online_mobile=1&user_id=${id}`
+        const data = {
+            online_mobile: 1,
+            user_id: id
+        }
         return await this.#getRequest('friends.getOnline', data)
     }
 
     // вовзращает маленькую аву пользователей + ФИО
     async getUsersMin(ids) {
-        const data = `&fields=photo_200&user_ids=${ids}`
+        const data = {
+            fields: 'photo_200',
+            user_ids: ids
+        }
         return await this.#getRequest('users.get', data)
     }
 
     // вовзращает расширенную инфу о пользователе
     async getFriendInfo(id) {
-        const data = `&fields=status,photo_200,bdate,city,blacklisted&user_ids=${id}`
+        const data = {
+            fields: 'status,photo_200,bdate,city,blacklisted',
+            user_ids: id
+        }
         return await this.#getRequest('users.get', data)
     }
 
     // возвращает когда последний раз пользователь был в сети
     async getLastOnlineUser(id = null) {
-        const data = `&fields=last_seen&user_ids=${id === null ? store.state.user.id : id}`
+        const data={
+            fields:'last_seen',
+            user_ids:id === null ? store.state.user.id : id
+        }
         return await this.#getRequest('users.get', data)
     }
 }
